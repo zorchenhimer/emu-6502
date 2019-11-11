fdf1a7a20533456b5810fcb71565dd5102763699
Zorchenhimer <zorchenhimer@gmail.com>
2019-11-10 21:01:57 -0500

    Fix some test stuff (it was borked)

--- core_test.go
+++ core_test.go
@@ -1,6 +1,7 @@
 package emu
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -30,7 +31,7 @@ type basicTest struct {
 	regExpected regState
 }
 
-func (bt basicTest) GetExpected() {
+func (bt basicTest) GetExpected() regState {
 	return bt.regExpected
 }
 
@@ -90,7 +91,7 @@ func TestImmediate(t *testing.T) {
 	core := newTestCore(t)
 	for _, bt := range testData_A {
 		t.Run(bt.name, func(t *testing.T) {
-			err := core.resetTest(bt.rom)
+			err := core.resetTest(t, bt.rom)
 			if err != nil {
 				t.Errorf("%s: %v", bt.name, err)
 			}
@@ -109,7 +110,7 @@ func TestImmediate(t *testing.T) {
 			}
 			ticksran -= 1 // remove the 0xFF "test done" tick
 
-			core.checkRegisters(t, bt.name, bt)
+			core.checkRegisters(t, bt.name, bt.regExpected)
 			t.Logf("ticks ran: %d", ticksran)
 			// TODO: phlags
 		})
@@ -132,7 +133,7 @@ type memTest struct {
 	regExpected regState
 }
 
-func (mt memTest) GetExpected() {
+func (mt memTest) GetExpected() regState {
 	return mt.regExpected
 }
 
@@ -141,69 +142,79 @@ func (mt memTest) GetExpected() {
 //}
 
 // Tests that check memory values in addition to register flags
-var memoryBased = memTest{
+var memoryBased = []memTest{
 	// STA
 	memTest{
 		"OP_STA_AB",
 		[]byte{OP_STA_AB, 0x00, 0x03},
 		memVal{0x0300, OP_STA_AB},
 		regState{a: OP_STA_AB},
-		regState{OP_STA_AB, 0x00, 0x00, 0x8002, 0x00}},
+		regState{OP_STA_AB, 0x00, 0x00, 0x8003, 0x00}},
+	memTest{
+		"OP_STA_ZP",
+		[]byte{OP_STA_ZP, 0x03},
+		memVal{0x0003, OP_STA_ZP},
+		regState{a: OP_STA_ZP},
+		regState{OP_STA_ZP, 0x00, 0x00, 0x8003, 0x00}},
 }
 
 func TestMemoryBased(t *testing.T) {
 	core := newTestCore(t)
 	for _, mt := range memoryBased {
 		t.Run(mt.name, func(t *testing.T) {
-			err := core.resetTest(mt.rom)
+			err := core.resetTest(t, mt.rom)
 			if err != nil {
-				t.Errorf("%s: %v", bt.name, err)
+				t.Errorf("%s: %v", mt.name, err)
 			}
 
-			core.setRegisters(bt.regInitial)
+			core.setRegisters(t, mt.regInitial)
+			ticksran := 0
 
 			for !core.testDone {
 				err = core.tick()
 				if err != nil {
-					t.Fatalf("%s: %v", bt.name, err)
+					t.Fatalf("%s: %v", mt.name, err)
 				}
 				ticksran++
 			}
 			ticksran -= 1 // remove the 0xFF "test done" tick
 
-			core.checkRegisters(t, mt)
+			core.checkRegisters(t, mt.name, mt.regExpected)
 
+			if core.ReadByte(mt.mem.addr) != mt.mem.val {
+				t.Errorf("%s: Incorrect memory value at $%04X: Exp:$%02X Got:$%02X", mt.name, mt.mem.addr, mt.mem.val, core.ReadByte(mt.mem.addr))
+			}
 		})
 	}
 }
 
 // Core helper functions
-func (core *Core) checkRegisters(t *testing.T, name string, e ExpectedResults) {
+func (core *Core) checkRegisters(t *testing.T, name string, e regState) {
 	t.Helper()
-	if core.A != e.GetExpected().a {
-		t.Errorf("%s: Incorrect A: Exp:$%02X Got:$%02X", name, e.GetExpected().a, core.A)
+	if core.A != e.a {
+		t.Errorf("%s: Incorrect A: Exp:$%02X Got:$%02X", name, e.a, core.A)
 	}
 
-	if core.X != e.GetExpected().x {
-		t.Errorf("%s: Incorrect X: Exp:$%02X Got:$%02X", name, e.GetExpected().x, core.X)
+	if core.X != e.x {
+		t.Errorf("%s: Incorrect X: Exp:$%02X Got:$%02X", name, e.x, core.X)
 	}
 
-	if core.Y != e.GetExpected().y {
-		t.Errorf("%s: Incorrect Y: Exp:$%02X Got:$%02X", name, e.GetExpected().y, core.Y)
+	if core.Y != e.y {
+		t.Errorf("%s: Incorrect Y: Exp:$%02X Got:$%02X", name, e.y, core.Y)
 	}
 
-	if core.PC != e.GetExpected().pc {
-		t.Errorf("%s: Incorrect PC: Exp:$%04X Got:$%04X", name, e.GetExpected().pc, core.PC)
+	if core.PC != e.pc {
+		t.Errorf("%s: Incorrect PC: Exp:$%04X Got:$%04X", name, e.pc, core.PC)
 	}
 }
 
 func (core *Core) setRegisters(t *testing.T, r regState) {
 	t.Helper()
-	core.A = bt.regInitial.a
-	core.X = bt.regInitial.x
-	core.Y = bt.regInitial.y
-	//core.PC = bt.regInitial.pc
-	core.Phlags = bt.regInitial.phlags
+	core.A = r.a
+	core.X = r.x
+	core.Y = r.y
+	//core.PC = r.pc
+	core.Phlags = r.phlags
 }
 
 func (c *Core) resetTest(t *testing.T, rom []byte) error {
