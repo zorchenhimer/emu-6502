b2e24ed799431823dc45f8f17a88084e9281e3d2
Zorchenhimer <zorchenhimer@gmail.com>
2020-02-14 01:24:20 -0500

    Add command to load a ROM; Add test ROM
    Added a simple command to load and execute a ROM.  Also added a test rom
    to load and execute.  The rom was built with a slightly modified linker
    configuration from the code ported to ca65 at
    https://github.com/amb5l/6502_65C02_functional_tests

---
+++ cmd/testrom.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/zorchenhimer/emu-6502"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Missing rom")
+		return
+	}
+
+	rom, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	core, err := emu.NewRWCore(rom, 0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// vectors have traps
+	core.PC = 0x8000
+	core.Debug = true
+
+	err = core.Run()
+	if err != nil {
+		fmt.Println(err)
+		core.DumpRegisters()
+		fmt.Printf("Ticks: %d\n", core.Ticks())
+		core.DumpPage(uint8(core.PC >> 8))
+		core.DumpMemoryToFile("dump.txt")
+		return
+	}
+}
