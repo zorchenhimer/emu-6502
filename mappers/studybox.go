ba1910f6ca085a354a5702d48a572bba1be38104
Zorchenhimer <zorchenhimer@gmail.com>
2022-05-29 17:03:38 -0400

    Start adding studybox support

---
+++ mappers/studybox.go
@@ -0,0 +1,277 @@
+package mappers
+
+import (
+	"io"
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+	"encoding/binary"
+
+	sbox "github.com/zorchenhimer/go-nes/studybox"
+)
+
+/*
+BRAM is a set of eight 4k banks (16k total) mapped at CPU $5xxx with
+$4400-$4fff being a mirror of the top 3k of bank 0.
+
+PRAM is a set of four 8k banks (32k total) mapped at CPU $6000-$7fff.
+*/
+
+func init() {
+	registerMapper(186, NewStudyBox)
+}
+
+type StudyBox struct {
+	rom []byte
+	mainRam [0x0800]byte
+	ramA [0x4000]byte
+	ramB [0x8000]byte
+	RamABank uint8
+	RamBBank uint8
+	PrgBank uint8
+
+	irqEnabled bool
+	tapePages [][]byte
+	currentPage int
+
+	tape *sbox.Studybox
+}
+
+type sbReadRegisterFunction func() uint8
+type sbWriteRegisterFunction func(value uint8)
+
+var sbReadRegisters map[uint16]sbReadRegisterFunction
+var sbWriteRegisters map[uint16]sbWriteRegisterFunction
+
+func NewStudyBox(raw []byte, hasRam bool) (Mapper, error) {
+	return &StudyBox{
+		rom: raw,
+		//mainRam: [0x0800]byte{},
+		//ramA: [0x8000]byte{},
+		//ramB: [0x8000]byte{},
+	}, nil
+}
+
+// Read a byte from tape
+func (sb *StudyBox) sbRead4200() uint8 {
+	//if sb.tape == nil {
+	//	panic("[sbRead4200()] No tape is opened")
+	//}
+	return 0
+}
+
+// Set RAM bank
+func (sb *StudyBox) sbWrite4200(value uint8) {
+	sb.RamABank = value & 0x03
+	sb.RamBBank = value & 0xC0
+	return
+}
+
+// Set ROM bank
+func (sb *StudyBox) sbWrite4201(value uint8) {
+	sb.PrgBank = value & 0x0F
+}
+
+// Tape drive status stuff
+func (sb *StudyBox) sbRead4201() uint8 {
+	// TODO
+	return 0
+}
+
+// Open a .studybox tape data file
+func (sb *StudyBox) OpenTape(filename string) error {
+
+	tape, err := os.Open(filename)
+	if err != nil {
+		return fmt.Errorf("Unable to open studybox tape data file: %w", err)
+	}
+	defer tape.Close()
+
+	sb.tapePages = [][]byte{}
+	buf := make([]byte, 4)
+	if _, err = tape.Read(buf); err != nil {
+		return err
+	}
+
+	if !bytes.Equal(buf, []byte("STBX")) {
+		return fmt.Errorf("Not a studybox file")
+	}
+
+	buf[0] = 0
+	buf[1] = 0
+	buf[2] = 0
+	buf[3] = 0
+
+	lenBin := make([]byte, 4)
+
+	for err == nil {
+		// read packet type
+		_, err = tape.Read(buf)
+		if err != nil {
+			break
+		}
+
+		_, err = tape.Read(lenBin)
+		if err != nil {
+			break
+		}
+		length := int64(binary.LittleEndian.Uint32(lenBin))
+
+		if bytes.Equal(buf, []byte("PAGE")) {
+			// decode the page
+			data := make([]byte, length)
+
+			// Skip past Audio offset lead in length and audio offset data length
+			_, err = tape.Seek(8, io.SeekCurrent)
+			if err != nil {
+				if err == io.EOF {
+					err = fmt.Errorf("premature after PAGE: %w", err)
+				}
+				break
+			}
+
+			n, err := tape.Read(data)
+			if err != nil {
+				if err == io.EOF && n != int(length) {
+					err = fmt.Errorf("not enough data read from tape.  expected %d read %d", length, n)
+				} else if err != io.EOF {
+					break
+				}
+			}
+
+			sb.tapePages = append(sb.tapePages, data)
+
+		} else if bytes.Equal(buf, []byte("AUDI")) {
+			// Skip this packet
+			_, err = tape.Seek(length, io.SeekCurrent)
+			if err == io.EOF {
+				err = fmt.Errorf("premature after AUDI: %w", err)
+			}
+		} else {
+			return fmt.Errorf("Unknown packet type: %s", buf)
+		}
+	}
+
+	if err != io.EOF {
+		return err
+	}
+
+	return nil
+}
+
+func (sb *StudyBox) ReadByte(address uint16) uint8 {
+	// Handle registers first
+	if reg, ok := sbReadRegisters[address]; ok {
+		return reg()
+	}
+
+	if address < 0x2000 {
+		return sb.mainRam[address % 0x0800]
+
+	} else if address < 0x4200 {
+		return 0
+
+	} else if address >= 0x4400 && address <= 0x4FFF {
+		// fixed RAM A
+		return sb.ramA[address % 0x1000]
+
+	} else if address >= 0x5000 && address <= 0x5FFF {
+		// Switched RAM A (32K)
+		return sb.ramA[address % 0x1000 + (uint16(sb.RamABank) * 0x1000)]
+
+	} else if address >= 0x6000 && address <= 0x7FFF {
+		// Switched RAM B (32k)
+		return sb.ramA[address % 0x2000 + (uint16(sb.RamBBank) * 0x2000)]
+
+	} else if address >= 0x8000 && address < 0xC000 {
+		// Switched ROM (256k)
+		return sb.rom[(address % 0x4000) + (uint16(sb.PrgBank) * 0x4000)]
+
+	} else if address >= 0xC000 {
+		// Fixed ROM bank 0
+		return sb.rom[address % 0x4000]
+
+	} else {
+		panic(fmt.Sprintf("Read from address that isn't mapped to anything: %04X", address))
+		// between 0x4203 and 0x4400, exclusively
+		// do nothing
+	}
+
+	return 0
+}
+
+func (sb *StudyBox) WriteByte(address uint16, value uint8) {
+	if reg, ok := sbWriteRegisters[address]; ok {
+		reg(value)
+		return
+	}
+
+	if address < 0x2000 {
+		sb.mainRam[address % 0x0800] = value
+
+	} else if address >= 0x4400 && address <= 0x4FFF {
+		// fixed RAM A
+		sb.ramA[address % 0x1000] = value
+
+	} else if address >= 0x5000 && address <= 0x5FFF {
+		// Switched RAM A (32K)
+		sb.ramA[address % 0x1000 + (uint16(sb.RamABank) * 0x1000)] = value
+
+	} else if address >= 0x6000 && address <= 0x7FFF {
+		// Switched RAM B (32k)
+		sb.ramA[address % 0x2000 + (uint16(sb.RamBBank) * 0x2000)] = value
+
+	//} else if address >= 0x8000 && address < 0xC000 {
+	//	// Switched ROM (256k)
+	//	sb.rom[(address % 0x4000) + (uint16(sb.PrgBank) * 0x4000)] = value
+
+	//} else if address >= 0xC000 {
+	//	// Fixed ROM bank 0
+	//	sb.rom[address % 0x4000] = value
+
+	} else {
+		//panic(fmt.Sprintf("Read from address that isn't mapped to anything: %04X", address))
+		// between 0x4203 and 0x4400, exclusively
+		// do nothing
+	}
+}
+
+func (sb *StudyBox) ReadWord(address uint16) uint16 {
+	return uint16(sb.ReadByte(address)) | (uint16(sb.ReadByte(address+1)) << 8)
+}
+
+func (sb *StudyBox) Offset(address uint16) uint32 {
+	panic("Offset not implemented yet")
+	return 0
+}
+
+func (sb *StudyBox) GetState() interface{} {
+	return nil
+}
+
+func (sb *StudyBox) SetState(data interface{}) error {
+	return fmt.Errorf("SetState() not implemented for StudyBox mapper")
+}
+
+func (sb StudyBox) Name() string {
+	return "StudyBox"
+}
+
+func (sb StudyBox) State() string {
+	return "State() not implemented for StudyBox mapper"
+}
+
+func (sb StudyBox) DumpFullStack() string {
+	st := []string{}
+	for i := 0; i < 256; i++ {
+		st = append(st, fmt.Sprintf("$%02X", sb.mainRam[0x100+i]))
+	}
+	return strings.Join(st, " ")
+}
+
+func (sb *StudyBox) ClearRam() {
+	sb.mainRam = [0x0800]byte{}
+	sb.ramA = [0x4000]byte{}
+	sb.ramB = [0x8000]byte{}
+}
